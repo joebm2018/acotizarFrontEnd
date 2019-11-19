89245b5a5f3ca17a7b8b54db087e8059eff08234
joe <volado23@gmail.com>
2019-11-18 21:11:25 -0500

    primer commit

---
+++ src/app/logged/components/mant/unidad-medida/unidad-medida.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unidad-medida',
+  templateUrl: './unidad-medida.component.html',
+  styles: []
+})
+export class UnidadMedidaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
